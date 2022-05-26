946271e5d7ca849b088106af0809534883cebd57
Tran Xuan Huong <phamhyta@gmail.com>
2022-05-26 22:22:32 +0700

    our_vision

--- landingpage/src/components/Regular-activities.js
+++ landingpage/src/components/Regular-activities.js
@@ -4,7 +4,7 @@ import frame_6_2 from '../images/frame_6_2.png';
 import frame_6_3 from '../images/frame_6_3.png';
 const RegularActivities = () => {
     return (
-        <div class="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 pt-10">
+        <div class="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 pt-10 text-xl">
             <div class="font-bold w-1/6 pl-6 py-2 text-2xl rounded-r-xl bg-pink-300 shadow-2xl">
                 Regular activities
             </div>
@@ -13,7 +13,7 @@ const RegularActivities = () => {
                     <div class="flex justify-center">
                         <img class="h-64" src={frame_6_1} alt="" />
                     </div>
-                    <div class="px-10 pb-6">
+                    <div class="px-6 pb-6">
                         <div class="font-bold text-xl text-center">Co-working </div>
                         <div>Members continously dicuss, share experience and contribute to develop products as well as ideas for competition.</div>
                     </div>
@@ -22,7 +22,7 @@ const RegularActivities = () => {
                     <div class="flex justify-center">
                         <img class="h-64" src={frame_6_2} alt="" />
                     </div>
-                    <div class="px-10 pb-6">
+                    <div class="px-6 pb-6">
                         <div class="font-bold text-xl text-center">Seminar</div>
                         <div>Involving in many seminars from big companies to get used to culture,environment and career prospect in working world.</div>
                     </div>
@@ -31,7 +31,7 @@ const RegularActivities = () => {
                     <div class="flex justify-center">
                         <img class="h-64" src={frame_6_3} alt="" />
                     </div>
-                    <div class="px-10 pb-6">
+                    <div class="px-6 pb-6">
                         <div class="font-bold text-xl text-center">Training </div>
                         <div>Trained by senior from different majjors each semester.</div>
                     </div>
