6ba6b7b1483e03b43d08d1433cc107b0b877dfa0
Trần Vân Ngọc <ngoctranvan214@gmail.com>
2022-05-29 23:17:38 +0700

    finish frame 1
    downloaded font 'jost

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -1,12 +1,15 @@
 import Contest from "./components/contest";
 import RegularActivities from "./components/Regular-activities";
 import OurVision from "./components/Our-vision";
+import Welcome from "./components/welcome"
 function App() {
   return (
     <div className="App">
+      <Welcome/>
       <OurVision/>
       <Contest/>
       <RegularActivities/>
+
     </div>
   );
 }
