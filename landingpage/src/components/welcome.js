66f7ecbdee655a7fbd71c037aeba27352abd4e5d
Trần Vân Ngọc <ngoctranvan214@gmail.com>
2022-05-30 11:56:09 +0700

    update frame 2 :V

--- landingpage/src/components/welcome.js
+++ landingpage/src/components/welcome.js
@@ -8,14 +8,14 @@ const Welcome = () => {
         <div class="bg-gradient-to-br from-amber-300 via-purple-500 to-blue-700">
             <div class="absolute rounded-full bg-blue-300 opacity-25" style={{width: '265px',
 height: '275px',left: '663px', top: '662px'}}></div>
-            <div class="absolute rounded-full bg-blue-300 opacity-25" style={{width: '267px',
+            <div class="absolute rounded-full bg-blue-300 opacity-25  " style={{width: '267px',
 height: '250px', top: '420px'}}></div>
             <div class="absolute rounded-full bg-yellow-300 opacity-25" style={{width: '231px',
 height: '251px', left:'620px',top: '170px'}}></div>
             <div class="flex">
                 <img class="ml-10" src={ SINNO_1}></img>
-                <div class="ml-5 mt-4">
-                    <p class="font-light" style={{fontSize:'40px'}}>Soict Innovation Club</p>
+                <div class="ml-5 mt-6">
+                    <p class="font-light text-5xl">Soict Innovation Club</p>
                 </div>
                 <div class="flex text-xl justify-between grid-cols-5 gap-x-12 ml-40 mt-10">
                     <a>Home</a>
@@ -25,12 +25,12 @@ height: '251px', left:'620px',top: '170px'}}></div>
                     <a>News</a>
                 </div>
             </div>
-            <div class="flex">
+            <div class="flex z-10">
                 <div class="ml-36" style={{marginTop:'166px',width:'433px'}}>
-                    <div>
+                    <div class="z-10">
                         <p class="font-bold text-white" style={{fontSize:'64px'}}>We are ready to change the world</p>
                     </div>
-                    <div class="rounded-3xl" style={{background:'#BEF81B',width: '365px',
+                    <div class="rounded-3xl z-10" style={{background:'#BEF81B',width: '365px',
     height: '82px', marginTop:'61px'}}>
                         <p class="font-bold text-4xl px-14 py-5">More about us</p>
                     </div>
